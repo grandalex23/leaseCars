9da3b002c97c2be5e821d37c1a4f8f0b80e6d972
grandalex23 <grandalex23@yandex.ru>
2024-04-08 21:31:49 +0300

    feat: add swiper-slide pagination

--- js/main.js
+++ js/main.js
@@ -1,5 +1,17 @@
+// autoplay - автосмена слайдов, основной код нах-ся в библиотеке
+
 const swiper = new Swiper(".swiper", {
+  speed: 2000,
   pagination: {
     el: ".swiper-pagination",
   },
+  autoplay: {
+    delay: 15000,
+    disableOnInteraction: false,
+  },
+  effect: "fade",
+  fadeEffect: {
+    crossFade: true
+  },
+
 });
